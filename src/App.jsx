fa7ade11e5d802298bd1ae08cee151b989e704bf
fritzkmanurung <89495965+fritzkmanurung@users.noreply.github.com>
2024-11-22 12:01:27 +0700

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -100,7 +100,7 @@ function App() {
       </header>
 
       {/* Main Content */}
-      <main className="px-36">
+      <main className="px-32">
         <section id="about" className="py-16"><About /></section>
         <section id="skills" className="py-16"><Skills /></section>
         <section id="projects" className="py-16"><Projects /></section>
